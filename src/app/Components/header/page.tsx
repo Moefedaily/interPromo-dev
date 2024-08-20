9c5df7db2d72420cd689f6f4cbfe486d4e59ef08
dimitri <dimitri.mauand.simplon@gmail.com>
2024-08-20 10:48:59 +0200

    premier

---
+++ src/app/Components/header/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        
+       <header className='bg-black'>
+        <h1 className='w-full text-5xl text-rose-400 font-bold text-center'>RESTAURANT LE SEOUL</h1>
+        <img src="" alt="" />
+        <nav className='w-full text-5xl text-rose-400 font-bold text-center'>
+            <a href="">Acceuil</a>
+            <a href="">La carte</a>
+            <a href="">Réservation</a>
+            <a href=""><div><p>connection</p></div></a>
+        </nav>
+       </header>
+    )
+}
+
