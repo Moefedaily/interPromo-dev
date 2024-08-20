badfd5aa21333db05d7eb6376d077903efcd6d92
Salome-Bx <salome.burteaux.simplon@gmail.com>
2024-08-20 10:28:08 +0200

    couleur ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex items-center justify-between p-24">
-        <div className="flex w-full bg-custom-grey">
-          <h1>My app works</h1>
+        <div className="flex w-full">
+          <h1>Page d'accueil</h1>
         </div>
     </main>
   );
