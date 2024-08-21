370d32a6b4cc3b0ee549bb7bf175e49e93990b6e
dimitri <dimitri.mauand.simplon@gmail.com>
2024-08-21 16:41:44 +0200

    premier

--- src/app/carte/page.tsx
+++ src/app/carte/page.tsx
@@ -9,14 +9,90 @@ import Seoul from "../page";
 const carte = ()  =>{
    const { push } = useRouter()
 return (
-   <main className='bg-custom-grey'>
+   <main className='bg-custom-grey min-h-screen  text-white'>
   <Header></Header>
-      <div className='flex items-center justify-between p-24'>
-        <p>carte</p>
-      </div>
+      <section className="flex justify-center items-center m-8">
+        <select className="bg-custom-grey  border border-white p-2
+         rounded-md" id="">
+          <option value="" disabled selected>Filtres</option>
+          <option value="">Plat ou dessert</option>
+          <option value="">Guabao</option>
+          <option value="">Bowls</option>
+          <option value="">Tapas</option>
+          <option value="">Veggie</option>
+        </select>
+      </section>
+      <section className="flex flex-wrap  p-24">
+        <div className="w-1/4 p-4">
+          <div >
+            <div className=" flex justify-center items-center ">
+             <img className="shadow-lg size-4/5 shadow-custom-pink rounded-lg" src="Guabao_bulgogi.png" alt="" />
+            </div>
+            <div className="flex space-x-2 justify-between mt-4">
+              <h2>Nom du plat</h2>
+              <p>8,90€</p>
+            </div>
+        
+            <p className="mt-4">Burger Asiatique à base de brioche de farine de riz, Poulet, Laitue, Carotte Marinée, Oignon Rouge, Mayonnaise Légèrement Pimentée, Cacahuètes, Coriandre</p>
+          </div>
+        </div>
+
+        <div className="w-1/4 p-4">
+          <div >
+            <div className=" flex justify-center items-center ">
+             <img className="shadow-lg size-4/5 shadow-custom-pink rounded-lg" src="Guabao_bulgogi.png" alt="" />
+            </div>
+            <div className="flex space-x-2 justify-between mt-4">
+              <h2>Nom du plat</h2>
+              <p>8,90€</p>
+            </div>
+        
+            <p className="mt-4">Burger Asiatique à base de brioche de farine de riz, Poulet, Laitue, Carotte Marinée, Oignon Rouge, Mayonnaise Légèrement Pimentée, Cacahuètes, Coriandre</p>
+          </div>
+        </div>
+        <div className="w-1/4 p-4">
+          <div >
+            <div className=" flex justify-center items-center ">
+             <img className="shadow-lg size-4/5 shadow-custom-pink rounded-lg" src="Guabao_bulgogi.png" alt="" />
+            </div>
+            <div className="flex space-x-2 justify-between mt-4">
+              <h2>Nom du plat</h2>
+              <p>8,90€</p>
+            </div>
+        
+            <p className="mt-4">Burger Asiatique à base de brioche de farine de riz, Poulet, Laitue, Carotte Marinée, Oignon Rouge, Mayonnaise Légèrement Pimentée, Cacahuètes, Coriandre</p>
+          </div>
+        </div>
+        <div className="w-1/4 p-4">
+          <div >
+            <div className=" flex justify-center items-center ">
+             <img className="shadow-lg size-4/5 shadow-custom-pink rounded-lg" src="Guabao_bulgogi.png" alt="" />
+            </div>
+            <div className="flex space-x-2 justify-between mt-4">
+              <h2>Nom du plat</h2>
+              <p>8,90€</p>
+            </div>
+        
+            <p className="mt-4">Burger Asiatique à base de brioche de farine de riz, Poulet, Laitue, Carotte Marinée, Oignon Rouge, Mayonnaise Légèrement Pimentée, Cacahuètes, Coriandre</p>
+          </div>
+        </div>
+        <div className="w-1/4 p-4">
+          <div >
+            <div className=" flex justify-center items-center ">
+             <img className="shadow-lg size-4/5 shadow-custom-pink rounded-lg" src="Guabao_bulgogi.png" alt="" />
+            </div>
+            <div className="flex space-x-2 justify-between mt-4">
+              <h2>Nom du plat</h2>
+              <p>8,90€</p>
+            </div>
+        
+            <p className="mt-4">Burger Asiatique à base de brioche de farine de riz, Poulet, Laitue, Carotte Marinée, Oignon Rouge, Mayonnaise Légèrement Pimentée, Cacahuètes, Coriandre</p>
+          </div>
+        </div>
+      </section>
   </main>
 )
 
 }
 
-export default Seoul
+export default carte
