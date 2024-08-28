86c1084db004cb237ba60b6daec49e3c4b9890da
unknown <mohammedfedaily@gmail.com>
2024-08-28 14:49:13 +0200

    feat:registarartion/profile

--- src/app/Components/NavButton/page.tsx
+++ src/app/Components/NavButton/page.tsx
@@ -6,7 +6,7 @@ interface NavButtonPropos {
 const NavButton = ({ onClick, children, className = "" }: NavButtonPropos) => (
   <button
     onClick={onClick}
-    className={`text-custom-pink font-semibold py-2 px-4 rounded transition duration-300 ease-in-out ${className}`}
+    className={`text-custom-pink font-semibold py-3 px-4 rounded transition duration-300 ease-in-out ${className}`}
   >
     {children}
   </button>
