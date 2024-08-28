1abe973d0e40a9c35d5557320d0368e42bc10835
unknown <mohammedfedaily@gmail.com>
2024-08-28 13:23:36 +0200

    feat:registarartion/profile

--- src/app/Components/NavButton/page.tsx
+++ src/app/Components/NavButton/page.tsx
@@ -1,9 +1,9 @@
-interface Props {
+interface NavButtonPropos {
   onClick: () => void;
   children: React.ReactNode;
   className?: string;
 }
-const NavButton = ({ onClick, children, className = "" }: Props) => (
+const NavButton = ({ onClick, children, className = "" }: NavButtonPropos) => (
   <button
     onClick={onClick}
     className={`text-custom-pink font-semibold py-2 px-4 rounded transition duration-300 ease-in-out ${className}`}
